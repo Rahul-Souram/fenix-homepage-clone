22fa4e3e699a2c15dba5e365e8614bd352062e1a
Rahul <souramrahul@gmail.com>
2023-04-28 22:11:29 +0530

    Added Alert

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ import LineGraph from "./components/LineGraph";
 function App() {
   return (
     <div className=" container-bg App">
+        <div
+          class="alert bg-safety-orange alert-dismissible fade show text-center"
+          role="alert"
+        >
+          <strong>Free 7 Day Trial Get Started Now</strong>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
       <div className="containers">
         <Header />
         <FirstComponent />
